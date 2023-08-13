74ebf8f84bd858755632657948bcc3a95b49c6eb
Riashad Hassan <109424665+RiashadHassan@users.noreply.github.com>
2023-08-13 14:10:11 +0600

    ready for submission

--- api/views.py
+++ api/views.py
@@ -6,14 +6,7 @@ from django.contrib.auth.models import User
 from datetime import datetime
 
 class InputItemAPI(APIView):
-    def get(self, request):
-        
-        
-        
-        '''takes params, checks if params are valid, then filters DB with the given params,
-            counts if there are any items in the DB within the DateTime range 
-            outputs response_data accordingly'''
-        
+    def get(self, request):              
         start_datetime = request.query_params.get('start_datetime')
         end_datetime = request.query_params.get('end_datetime')
         user_id = request.query_params.get('user_id')
@@ -25,15 +18,18 @@ class InputItemAPI(APIView):
             context = {'status': 'error', 'error_response': 'User does not exist'}
             return Response(context, status=404)
         
-        try: #formatting the DateTime range
+        try: #for better error response if datetime format isn't correct
             start_datetime = datetime.strptime(start_datetime, "%Y-%m-%d %H:%M:%S")
             end_datetime = datetime.strptime(end_datetime, "%Y-%m-%d %H:%M:%S")
-        except ValueError:
+        except ValueError: 
             context = {'status': 'error', 'error_response': 'date format is not valid'}
             return Response(context, status=400)
         
         #retieving input items within the time range & ordering them by the newest ones first
-        input_items = InputItem.objects.filter(user=user, input_timestamp__range=(start_datetime, end_datetime)).order_by('-input_timestamp')   
+        input_items = InputItem.objects.filter\
+            (user=user, input_timestamp__range=(start_datetime, end_datetime))\
+                .order_by('-input_timestamp')  
+                 
         input_items_count= input_items.count()
         serialized_input_items = InputItemSerializer(input_items, many= True) 
                 
